4653e7f416d3f6dbb3245f9612e55ea56d5dff70
A1-18\A1-18 <shong1332@naver.com>
2025-12-03 16:57:07 +0900

    no message

---
+++ src/main/java/com/devwork/memo/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.devwork.memo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Hello, World!!";
+    }
+}
