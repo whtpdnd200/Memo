7b046fddb916635da544b6d087aa415886c93fd8
whtpdnd <whtpdnd@200gmail.com>
2025-12-03 18:39:02 +0900

    [조세웅] - 회원가입 화면 구성 완료 #1

---
+++ src/main/java/com/devwork/memo/user/UserController.java
@@ -0,0 +1,16 @@
+package com.devwork.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/join")
+    public String join() {
+
+        return "/user/join";
+    }
+}
